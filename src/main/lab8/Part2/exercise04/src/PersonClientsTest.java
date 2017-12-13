2056e9913a8ec01d745154cdcc4a1d10bc791def
Yevhen Shymko <imjust93@gmail.com>
2017-12-13 17:35:38 +0100

    EX4 test devided it propper methods

--- src/main/lab8/Part2/exercise04/src/PersonClientsTest.java
+++ src/main/lab8/Part2/exercise04/src/PersonClientsTest.java
@@ -2,6 +2,8 @@ package lab8.Part2.exercise04.src;// The Person class has multiple clients, but 
 // one file for convenience.  Imagine them as non-test methods in separate 
 // client classes.
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
@@ -10,31 +12,51 @@ import static junit.framework.Assert.assertEquals;
 
 public class PersonClientsTest {
 
-	@Test
-	public void testClients() throws IOException {
-		Person bobSmith = new Person("Smith", "Bob", null);
-		Person jennyJJones = new Person("Jones", "Jenny", "J");
+    private Person bobSmith;
+    private Person jennyJJones;
 
-		StringWriter out = new StringWriter();
-		Client1.printPerson(out, bobSmith);
-		assertEquals("Bob Smith", out.toString());
+    @Before
+    public void inti() {
+        bobSmith = new Person("Smith", "Bob", null);
+        jennyJJones = new Person("Jones", "Jenny", "J");
+    }
 
-		out = new StringWriter();
-		Client1.printPerson(out, jennyJJones);
-		assertEquals("Jenny J Jones", out.toString());
+    @Test
+    public void testClientsToStringBobAndJenny() throws IOException {
+        assertEquals("Smith, Bob", Client4.toString(bobSmith));
+        assertEquals("Jones, Jenny J", Client4.toString(jennyJJones));
+    }
+    @Test
+    public void testClientDisplayJenny() throws IOException {
+        StringWriter out = new StringWriter();
+        Client3.display(out, jennyJJones);
+        assertEquals("Jones, Jenny J", out.toString());
+    }
 
-		assertEquals("Smith, Bob", Client2.formatPerson(bobSmith));
-		assertEquals("Jones, Jenny J", Client2.formatPerson(jennyJJones));
+    @Test
+    public void testClientDisplayBob() throws IOException {
+        StringWriter out = new StringWriter();
+        Client3.display(out, bobSmith);
+        assertEquals("Smith, Bob", out.toString());
+    }
 
-		out = new StringWriter();
-		Client3.display(out, bobSmith);
-		assertEquals("Smith, Bob", out.toString());
+    @Test
+    public void testClientFromat() {
+        assertEquals("Smith, Bob", Client2.formatPerson(bobSmith));
+        assertEquals("Jones, Jenny J", Client2.formatPerson(jennyJJones));
+    }
 
-		out = new StringWriter();
-		Client3.display(out, jennyJJones);
-		assertEquals("Jones, Jenny J", out.toString());
+    @Test
+    public void testClientWriter() throws IOException {
+        StringWriter out = new StringWriter();
+        Client1.printPerson(out, jennyJJones);
+        assertEquals("Jenny J Jones", out.toString());
+    }
 
-		assertEquals("Smith, Bob", Client4.toString(bobSmith));
-		assertEquals("Jones, Jenny J", Client4.toString(jennyJJones));
-	}
+    @Test
+    public void testClientBobSmith() throws IOException {
+        StringWriter out = new StringWriter();
+        Client1.printPerson(out, bobSmith);
+        assertEquals("Bob Smith", out.toString());
+    }
 }
