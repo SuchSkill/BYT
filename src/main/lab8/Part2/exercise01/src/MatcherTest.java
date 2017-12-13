091162069b5b94aa57eb4a01540664c8e7aaf886
Yevhen Shymko <imjust93@gmail.com>
2017-12-13 16:45:45 +0100

    Divided tests into apropriate methods

--- src/main/lab8/Part2/exercise01/src/MatcherTest.java
+++ src/main/lab8/Part2/exercise01/src/MatcherTest.java
@@ -1,5 +1,6 @@
 package lab8.Part2.exercise01.src;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -7,22 +8,31 @@ import static org.junit.Assert.assertTrue;
 
 public class MatcherTest {
 
+	private Matcher matcher;
+	private int[] expected;
+	private int clipLimit;
+	private int delta;
+
+	@BeforeClass
+	public void init(){
+		matcher = new Matcher();
+		expected = new int[] { 10, 50, 30, 98 };
+		clipLimit = 100;
+		delta = 5;
+	}
 	@Test
-	public void testMatch() {
-		Matcher matcher = new Matcher();
-
-		int[] expected = new int[] { 10, 50, 30, 98 };
-		int clipLimit = 100;
-		int delta = 5;
-
+	public void testDataFail1() {
+		int[] actual = new int[] { 10, 60, 30, 98 };
+		assertTrue(!matcher.match(expected, actual, clipLimit, delta));
+	}
+	@Test
+	public void testDataFail2() {
+		int[] actual = new int[] { 10, 50, 30 };
+		assertTrue(!matcher.match(expected, actual, clipLimit, delta));
+	}
+	@Test
+	public void testDataOkay() {
 		int[] actual = new int[] { 12, 55, 25, 110 };
-
 		assertTrue(matcher.match(expected, actual, clipLimit, delta));
-
-		actual = new int[] { 10, 60, 30, 98 };
-		assertTrue(!matcher.match(expected, actual, clipLimit, delta));
-
-		actual = new int[] { 10, 50, 30 };
-		assertTrue(!matcher.match(expected, actual, clipLimit, delta));
 	}
 }
